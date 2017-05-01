43ea76f84b00da4ef915d84e5f0566968725ce3c
fockedeym <martin.fockedey@student.uclouvain.be>
2017-05-01 18:27:39 +0200

    Create menu.java

---
+++ code/EZMeal/app/src/main/java/be/lsinf1225/ezmeal/menu.java
@@ -0,0 +1,55 @@
+package be.lsinf1225.ezmeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class menu extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1 ;
+    private Button ButtonSearch ;
+    private Button ButtonCatalogue ;
+    private Button ButtonSuggest ;
+    //private Button ButtonUser ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        ButtonSearch = (Button) findViewById(R.id.button_search);
+        ButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(menu.this, search.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
+        ButtonCatalogue = (Button) findViewById(R.id.button_catalogue);
+        ButtonCatalogue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(menu.this, catalogue.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
+        ButtonSuggest = (Button) findViewById(R.id.button_suggest);
+        ButtonSuggest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(menu.this, suggest.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
+        /*ButtonUser = (Button) findViewById(R.id.button_user);
+        ButtonUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(menu.this, menu.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });*/
+    }
+
+}
+
